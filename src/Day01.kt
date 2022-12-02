baa26c562316588f2b9bd3bab203c81e3f24a69b
jpatino37 <jpatino3792@gmail.com>
2022-12-02 13:12:14 -0500

    day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,51 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var mostCalories = 0
+        var calorieCount = 0
+        for (i in input) {
+            if (i.isNotEmpty()) {
+                calorieCount += i.toInt()
+            } else {
+                if (calorieCount > mostCalories) {
+                    mostCalories = calorieCount
+                    calorieCount = 0
+                } else {
+                    calorieCount = 0
+                }
+            }
+        }
+        return mostCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calorieList = mutableListOf<Int>()
+        var calorieCount = 0
+
+        for (i in input.indices) {
+            if (input[i].isNotEmpty()) {
+                calorieCount += input[i].toInt()
+                if (i == input.size - 1) {
+                    calorieList.add(calorieCount)
+                }
+            }  else {
+                calorieList.add(calorieCount)
+                calorieCount = 0
+            }
+        }
+
+        calorieList.sort()
+        val lastThree = calorieList.takeLast(3)
+        return lastThree.sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01Sample")
+    //println(part1(testInput))
+    //println(part2(testInput))
 
     val input = readInput("Day01")
-    println(part1(input))
+    //println("RUNNING PART 1")
+    //println(part1(input))
+    println("RUNNING PART 2")
     println(part2(input))
 }
