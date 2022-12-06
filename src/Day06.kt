d429a5fff7ddc3f533d0854e515c2ba4b0d461b0
jpatino37 <jpatino3792@gmail.com>
2022-12-06 01:57:09 -0500

    Day 06 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,18 +1,50 @@
 fun main() {
+    fun isDistinct(str: String) = str.toList().size == str.toList().toHashSet().size
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val charList = input[0]
+        var marker = 0
+        var counter = 2
+        var listStart = 0
+        while (counter < charList.length) {
+            val window = charList.substring(listStart, counter + 2)
+            if (isDistinct(window)) {
+                marker = counter + 2
+                break
+            } else {
+                counter++
+                listStart++
+            }
+        }
+
+        return marker
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val charList = input[0]
+        var marker = 0
+        var counter = 12
+        var listStart = 0
+        while (counter < charList.length) {
+            val window = charList.substring(listStart, counter + 2)
+            if (isDistinct(window)) {
+                marker = counter + 2
+                break
+            } else {
+                counter++
+                listStart++
+            }
+        }
+
+        return marker
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day06Sample")
-    println(part1(testInput))
+    //val testInput = readInput("Day06Sample")
+    //println(part1(testInput))
     //println(part2(testInput))
 
-    //val input = readInput("Day06")
+    val input = readInput("Day06")
     //println(part1(input))
-    //println(part2(input))
+    println(part2(input))
 }
